951d24b8617c4eb312fb038b577602e31d14bdb2
Zentro-Works <zw.zentro@gmail.com>
2026-06-17 15:47:10 +0530

    Add MongoDB Config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 
-module.exports = function connectDB() {
-  return mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => {
-      console.error('MongoDB connection error:', err.message)
-      process.exit(1)
+module.exports = async function connectDB() {
+  try {
+    console.log('Connecting to MongoDB...')
+    await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000
     })
+    console.log('✅ MongoDB connected')
+  } catch (err) {
+    console.error('❌ MongoDB connection error:')
+    console.error(err)
+    throw err
+  }
 }
